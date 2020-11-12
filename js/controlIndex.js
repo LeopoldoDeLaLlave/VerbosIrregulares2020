2e9da3c1bfb8e57a1e666021614bdbbaffa74dd1
Javier de la Llave <jdelallalor@gmail.com>
2020-11-12 19:09:46 +0100

    Puesta puntuación, fin de la partida y cambio de nivel

--- js/controlIndex.js
+++ js/controlIndex.js
@@ -14,6 +14,9 @@ var arrayNumero = [];
 //Guarda la pregunta que se está viendo 
 var preguntaActual = 0;
 
+//Nos indica que respuesta esta oculta
+var respuestaOculta;
+
 //Nos va a crear un array con tantos números como preguntas haya
 function crearArray() {
     for (let i = 0; i < verbos.length; i++) {
@@ -44,7 +47,7 @@ function desordenarArray() {
 //Abre una pregunta
 function abrir() {
     nPreguntas = $(this).text();
-    $("#headerNumeroPreguntas").text(nPreguntas);
+    $("#headerNumeroPreguntas").text("0/"+nPreguntas);
     //Tenemos que cargarlo primero para que la función nueva pregunta exista
     $('#principal').load('partida.html');
 
@@ -55,11 +58,33 @@ function nuevaPregunta() {
     //Ponemos el verbo en español en la pregunta
     $('#verboEsp').text(verbos[arrayNumero[preguntaActual]][3]);
     //Ponemos los verbos en inglés
-    $('#base').text(verbos[arrayNumero[preguntaActual]][0]);
-    $('#pasado').text(verbos[arrayNumero[preguntaActual]][1]);
-    $('#participio').text(verbos[arrayNumero[preguntaActual]][2]);
-
+    $('#soluciones').html("");
+    $('#soluciones').append('<button type="button" class="btn btn-warning btn-lg" id="base">'+verbos[arrayNumero[preguntaActual]][0]+'</button>');
+    $('#soluciones').append('<button type="button" class="btn btn-warning btn-lg" id="pasado">'+verbos[arrayNumero[preguntaActual]][1]+'</button>');
+    $('#soluciones').append('<button type="button" class="btn btn-warning btn-lg" id="participio">'+verbos[arrayNumero[preguntaActual]][2]+'</button>');
+    quitarUnaOpcion();
+    //Ponemos cuantas preguntas lleva
+    $("#headerNumeroPreguntas").text(preguntaActual+"/"+nPreguntas);
 };
+
+//Cambiamos una de las respuestas por un hueco en blanco para que conteste el usuario
+function quitarUnaOpcion(){
+   respuestaOculta = Math.floor(Math.random() * 2);
+
+    if(respuestaOculta == 0){
+        $('#base').html("");
+        $('#base').append('<input type="text" id="respuesta" >');
+        
+    }else if(respuestaOculta == 1){
+        $('#pasado').html("");
+        $('#pasado').append('<input type="text" id="respuesta" >');
+    }else{
+        $('#participio').html("");
+        $('#participio').append('<input type="text" id="respuesta" >');
+    }
+
+    
+}
 
 
 //pasa a la siguiente pregunta
@@ -67,5 +92,30 @@ function siguiente() {
     preguntaActual++;
     nuevaPregunta();
 }
+
+
+//Comprueba si la respuesta es correcta
+function comprobar(){
+
+    //Si acierta ponemos la siguiente pregunta
+    if($('#respuesta').val().localeCompare(verbos[arrayNumero[preguntaActual]][respuestaOculta])==0){
+        //Si no ha terminado las preguntas pasamos a la siguiente
+        if(preguntaActual<nPreguntas){
+            siguiente();
+        }else{
+          alert("You win!");
+          $("#comprobar").attr("disabled","disabled");
+        }
+        
+    }else{//Si falla ponemos la respuesta correcta y cambiamos el botón
+        $('#comprobar').text(verbos[arrayNumero[preguntaActual]][respuestaOculta]);
+        $('#comprobar').removeClass();
+        $('#comprobar').addClass("btn btn-danger btn-lg");
+        $("#comprobar").attr("disabled","disabled");
+        alert("You lose :(");
+    }
+}
+
+
 
 
--- js/controlPartida.js
+++ js/controlPartida.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
 
-    $('#comprobar').click(siguiente);
+    $('#comprobar').click(comprobar);
+    
     nuevaPregunta();
 
 });
